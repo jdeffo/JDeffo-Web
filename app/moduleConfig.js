4f90606f565b94be12a752b5bffc3eaa0181f723
Jeremy DeFosset <jsd6695@gmail.com>
2016-12-21 01:10:47 -0500

    Finished Routing Created 1st iteration Navigation w/ Bootstrap Began the Home Screen

---
+++ app/moduleConfig.js
@@ -0,0 +1,35 @@
+var JDeffoApp = angular.module('JDeffoApp', ['ngRoute']);
+
+JDeffoApp.config(['$routeProvider', function($routeProvider){
+
+  $routeProvider
+    .when('/home', {
+      templateUrl: 'Views/Home.html'
+      //controller: 'HomeController',
+      //caseInsensitiveMatch: true
+    })
+    /*.when('/about', {
+      templateUrl: 'Views/About.html',
+      controller: 'AboutController',
+      caseInsensitiveMatch: true
+    })
+    .when('/contact', {
+      templateUrl: 'Views/Contact.html',
+      controller: 'ContactController',
+      caseInsensitiveMatch: true
+    })
+    .when('/portfolio', {
+      templateUrl: 'Views/Portfolio.html',
+      controller: 'PortfolioControler',
+      caseInsensitiveMatch: true
+    })
+    .when('/resume', {
+      templateUrl: 'Views/Resume.html',
+      controller: 'ResumeController',
+      caseInsensitiveMatch: true
+    })*/
+    .otherwise({
+      redirectTo: '/home'
+    });
+
+}]);
